82630a0e6f12643644cc053a20d07486c742b597
phannam1405 <phannam1405@gmail.com>
2025-03-08 12:51:26 +0700

    xong_login

--- app/src/main/java/com/example/dacs3/LoginActivity.kt
+++ app/src/main/java/com/example/dacs3/LoginActivity.kt
@@ -1,14 +1,22 @@
 package com.example.dacs3
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.example.dacs3.databinding.ActivityLoginBinding
 
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.txtCA.setOnClickListener{
+            val i1 = Intent(this, RegisterActivity::class.java)
+            startActivity(i1)
+        }
     }
 }
